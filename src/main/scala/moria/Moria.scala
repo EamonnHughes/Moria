fa1d3531b8685e35abdb0675918b8e1970e4dd4e
arathorn2 <43751985+arathorn2@users.noreply.github.com>
2021-07-15 12:35:27 -0400

    Update Moria.scala

--- src/main/scala/moria/Moria.scala
+++ src/main/scala/moria/Moria.scala
@@ -13,10 +13,39 @@ class Moria extends PApplet {
     rect(mazeEnterX, mazeEnterY, entx, enty)
     rect(mr1x, mr1y, r1x, r1y)
     rect(mr2x, mr2y, r1y, r2y)
+    connectLine(mr1x, mr2x, mr1y, mr2y, r1x, r2x, r1y, r2y)
   }
 
-  def connectLine(): Unit = {
-    //connect a line
+  def connectLine(
+      x1: Int,
+      x2: Int,
+      y1: Int,
+      y2: Int,
+      xl1: Int,
+      xl2: Int,
+      yl1: Int,
+      yl2: Int
+  ): Unit = {
+    var xf1: Int = x1
+    var xf2: Int = x2
+    var yf1: Int = y1
+    var yf2: Int = y2
+    if (x1 > x2) {
+      xf2 = x2 - xl2
+    } else if (x1 < x2) {
+      xf1 = x1 + xl1
+    } else {
+      xf1 = x1 + (xl1 / 2)
+      xf2 = x2 + (xl2 / 2)
+    }
+    if (y1 > y2) {
+      yf2 = y2 - yl2
+    } else if (y1 < y2) {
+      yf1 = y1 + yl1
+    }
+
+    line(xf1, yf1, xf2, yf2)
+
   }
 }
 
