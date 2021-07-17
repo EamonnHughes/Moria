d3f233701caeb73890d9d0aa2d96c88903c3c28f
arathorn2 <43751985+arathorn2@users.noreply.github.com>
2021-07-17 10:12:57 -0400

    Update roomdrawer.scala

--- src/main/scala/moria/roomdrawer.scala
+++ src/main/scala/moria/roomdrawer.scala
@@ -1,6 +1,10 @@
 package moria
 
 object roomdrawer {
-  def xaxis(): Unit = {}
+  def xaxis(): Unit = {
+    for (i <- 0 to 10) {
+      println("hello world")
+    }
+  }
   def yaxis(): Unit = {}
 }
