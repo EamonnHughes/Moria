0bfa2bb18770217f7df59ea7a9ef84ddd137a816
arathorn2 <43751985+arathorn2@users.noreply.github.com>
2022-01-04 18:07:43 -0500

    bs

--- src/main/scala/moria/Moria.scala
+++ src/main/scala/moria/Moria.scala
@@ -2,7 +2,16 @@ package moria
 
 import processing.core._
 
-class Moria extends PApplet {}
+class Moria extends PApplet {
+  val BoardWidth = 1024
+  val BoardHeight = 512
+
+  override def settings(): Unit = {
+    size(BoardWidth, BoardHeight)
+  }
+
+  override def draw(): Unit = {}
+}
 
 object Moria extends App {
   PApplet.main(classOf[Moria].getName)
---
+++ src/main/scala/moria/Room.scala
@@ -0,0 +1,5 @@
+package moria
+
+case class Room(posX: Int, posY: Int, lX: Int, lY: Int) {
+  def drawRoom(): Unit = {}
+}
