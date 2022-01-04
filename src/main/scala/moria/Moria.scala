82c2bbf0f51a97e0f226e58f063647f55f1607d4
arathorn2 <43751985+arathorn2@users.noreply.github.com>
2022-01-04 18:52:12 -0500

    ee

--- src/main/scala/moria/Moria.scala
+++ src/main/scala/moria/Moria.scala
@@ -1,16 +1,36 @@
 package moria
 
 import processing.core._
+import processing.event.MouseEvent
 
 class Moria extends PApplet {
   val BoardWidth = 1024
   val BoardHeight = 512
+  var cPosX = 64.0f
+  var cPosY = 64.0f
+  var r1 =
+    List(Room(32, 32, 64, 128), Room(128, 32, 64, 128), Room(32, 256, 64, 32))
 
   override def settings(): Unit = {
     size(BoardWidth, BoardHeight)
   }
 
-  override def draw(): Unit = {}
+  override def draw(): Unit = {
+    background(5, 0, 155)
+    r1.foreach { room =>
+      room.drawRoom(this)
+    }
+    ellipse(cPosX, cPosY, 16, 16)
+    ellipse(mouseX, mouseY, 4, 4)
+    color(255, 55, 55)
+  }
+
+  override def mouseClicked(event: MouseEvent): Unit = {
+    if (r1.exists(r => r.isInside(event.getX, event.getY))) {
+      cPosX = ((event.getX / 16).floor) * 16
+      cPosY = ((event.getY / 16).ceil) * 16
+    }
+  }
 }
 
 object Moria extends App {
--- src/main/scala/moria/Room.scala
+++ src/main/scala/moria/Room.scala
@@ -1,5 +1,17 @@
 package moria
 
+import processing.core._
+
 case class Room(posX: Int, posY: Int, lX: Int, lY: Int) {
-  def drawRoom(): Unit = {}
+  def drawRoom(p: PApplet): Unit = {
+    p.rect(posX, posY, lX, lY)
+
+  }
+  def isInside(x: Float, y: Float): Boolean = {
+    x > posX &&
+    x < posX + lX &&
+    y > posY &&
+    y < posY + lY
+  }
+
 }
