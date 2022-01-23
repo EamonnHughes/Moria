88531d9e49632dd3644552ad6151154c6581ba24
arathorn2 <43751985+arathorn2@users.noreply.github.com>
2022-01-22 21:32:26 -0500

    statistics

---
+++ src/main/scala/moria/Stats.scala
@@ -0,0 +1,21 @@
+package moria
+
+trait Stats {
+  var bStrength: Int
+  var bFinesse: Int
+  var bFortitude: Int
+  var bEnergy: Int
+  var bIntelligence: Int
+  var bPersuasion: Int
+  var bIntimidation: Int
+  var bRationalism: Int
+  var Strength: Int
+  var Finesse: Int
+  var Fortitude: Int
+  var Energy: Int
+  var Intelligence: Int
+  var Persuasion: Int
+  var Intimidation: Int
+  var Rationalism: Int
+
+}
