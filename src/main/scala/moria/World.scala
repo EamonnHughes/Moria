de9efc7031171a0669469366699aabfba2cb01a6
arathorn2 <efohughes@gmail.com>
2022-02-01 19:30:06 -0500

    Update World.scala

--- src/main/scala/moria/World.scala
+++ src/main/scala/moria/World.scala
@@ -16,6 +16,7 @@ object World {
   var gear = List(
     gearItem("Helm", "Iron", 1, 1, 1, 1, "Boring")
   )
+  var projectilesList = List.empty[Projectile]
   var player: Player = Player(
     Location(4, 4),
     Location(4, 4),
