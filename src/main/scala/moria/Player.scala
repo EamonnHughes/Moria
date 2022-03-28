6d8158f0794af0d8c0d205b1ff5be2fc79791354
arathorn2 <efohughes@gmail.com>
2022-03-28 16:51:51 -0400

    Update Player.scala

--- src/main/scala/moria/Player.scala
+++ src/main/scala/moria/Player.scala
@@ -3,6 +3,9 @@ package moria
 import processing.core.PApplet
 
 case class Player(var loc: Location) extends Thing {
+  //var pathToDest = Option.empty[Path]
+
+  def navigateTo(loc: Location): Unit = {}
   def draw(p: PApplet): Unit = {
     p.fill(0, 255, 0)
     p.rect(loc.x * 16, loc.y * 16, 16, 16)
