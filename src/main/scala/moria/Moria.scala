cd8f7ced30cad24b7d2222516373813ea5a21263
arathorn2 <efohughes@gmail.com>
2022-03-28 15:32:35 -0400

    Update Moria.scala

--- src/main/scala/moria/Moria.scala
+++ src/main/scala/moria/Moria.scala
@@ -2,7 +2,17 @@ package moria
 
 import processing.core.PApplet
 
-class Moria extends PApplet {}
+class Moria extends PApplet {
+  override def setup(): Unit = {}
+  override def settings(): Unit = {
+    size(1024, 512)
+    noSmooth()
+  }
+  override def draw(): Unit = {
+    background(100, 100, 100)
+  }
+
+}
 object Moria extends App {
   PApplet.main(classOf[Moria])
 
