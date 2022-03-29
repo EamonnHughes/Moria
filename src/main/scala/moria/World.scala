b80eb6b83a5f8096f0f3654e70e09ec77aaa51c7
arathorn2 <efohughes@gmail.com>
2022-03-29 09:08:38 -0400

    Update World.scala

--- src/main/scala/moria/World.scala
+++ src/main/scala/moria/World.scala
@@ -2,7 +2,7 @@ package moria
 
 object World {
   var player = Player(Location(4, 4), Location(4, 12))
-  var rooms = List(Room(Location(2, 2), 8, 8), Room(Location(2, 12), 4, 4))
+  var rooms = List(Room(Location(2, 2), 24, 24))
   var listOfThings = List(player)
   def findSquad(location: Location): Thing = { // nulls
     listOfThings.find(thing => thing.loc == location).orNull
