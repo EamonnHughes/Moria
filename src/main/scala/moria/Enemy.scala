4f3bcfc661ca73a545b2c57d964c288e4bf2cf2f
arathorn2 <efohughes@gmail.com>
2022-04-03 15:37:38 -0400

    Update Enemy.scala

--- src/main/scala/moria/Enemy.scala
+++ src/main/scala/moria/Enemy.scala
@@ -2,7 +2,7 @@ package moria
 
 import processing.core.PApplet
 
-case class Enemy(loc: Location) extends Thing {
+case class Enemy(var loc: Location) extends Thing {
   def draw(p: PApplet): Unit = {
     p.fill(255, 0, 0)
     p.rect(loc.x * 16, loc.y * 16, 16, 16)
