18c0fdbd8a76303ba161b81f4df4266c3d193bbe
arathorn2 <efohughes@gmail.com>
2022-04-03 17:38:07 -0400

    e

--- src/main/scala/moria/Enemy.scala
+++ src/main/scala/moria/Enemy.scala
@@ -3,8 +3,18 @@ package moria
 import processing.core.PApplet
 
 case class Enemy(var loc: Location) extends Thing {
+  var distanceFromPlayer = 100
   def draw(p: PApplet): Unit = {
     p.fill(255, 0, 0)
     p.rect(loc.x * 16, loc.y * 16, 16, 16)
+    distanceFromPlayer = Math
+      .sqrt(
+        (Math.abs(loc.x - World.player.loc.x) * Math
+          .abs(loc.x - World.player.loc.x)) + Math
+          .abs(loc.y - World.player.loc.y) * Math
+          .abs(loc.y - World.player.loc.y)
+      )
+      .toInt
   }
+  def chooseTarget(): Unit = {}
 }
